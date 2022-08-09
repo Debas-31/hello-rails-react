23e61e5b5bad327ba425459d094e8058ab49ff7a
debas-31 <debas31@gmail.com>
2022-08-09 18:57:07 +0300

    Remove boiler plate views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :greetings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      get 'greetings', to: 'greetings#index'
+    end
+  end
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static#index"
 end
