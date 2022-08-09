1fd993fedc2155a02c47e8a9d89719827ab027e5
debas-31 <debas31@gmail.com>
2022-08-09 19:52:25 +0300

    Add Home and Header component

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,12 +1,36 @@
-import React from "react"
-import PropTypes from "prop-types"
-class Header extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Header = ()=>{
+  const links = [
+    {
+      text: 'Home',
+      path: '/',
+      id: 0,
+    },
+    {
+      text: 'Greeting',
+      path: '/greeting',
+      id: 1,
+    }
+  ];
+  return (
+    <header className="">
+      <a href="/" className="">Greetings</a>
+
+      <nav>
+        <ul className="">
+          {
+          links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} className="">{link.text}</NavLink>
+            </li>
+          ))
+        }
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,12 +1,13 @@
-import React from "react"
-import PropTypes from "prop-types"
-class Home extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Home =()=>{
+  return (
+    <div class="">
+      <p className="">Welcome To Hello Rails React App</p>
+      <NavLink to="/greeting" className="">Goto Greeting</NavLink>
+    </div>
+  )
 }
 
 export default Home
